889242606ade787853f16436a990d7c73b586fa8
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2024-10-18 14:35:20 +0530

    Add files via upload

---
+++ Program 1.py
@@ -0,0 +1,11 @@
+'''
+Author: Joshil S Abraham
+Date: 18/10/2024
+Description: Program that asks the user to input their name and age.
+             After receiving the input, display a message that includes
+             the user's name and age.
+'''
+name=input("Enter your name:")
+age=input("Enter your age:")
+print("Hello, ",name,"You are ",age,"years old")
+
