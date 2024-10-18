4522db4dc52fe8a798685686b3a5a1d39bf3286e
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2024-10-18 15:03:03 +0530

    Update Program 2.py

--- Program 2.py
+++ Program 2.py
@@ -5,7 +5,7 @@ Description: Program that stores a string in a variable.
              Extract a specific part of the string (substring) and then concatenate it with another string.
              Finally, display the new string.
 '''
-string_1="Hello,World!"
+string_1="Hello, World!"
 substring=string_1[6:11]
 string_2="Everyone!"
 concatenated_string=substring+string_2
