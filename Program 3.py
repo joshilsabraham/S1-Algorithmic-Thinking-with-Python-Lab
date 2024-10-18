12a3f95b57311c13f5e2e87df9f2fabab49d9d85
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2024-10-18 14:59:40 +0530

    Add files via upload

---
+++ Program 3.py
@@ -0,0 +1,20 @@
+'''
+Author: Joshil S Abraham
+Date: 18 / 10 / 2024
+Description: Program that demonstrates the usage of arithmetic,
+             comparison, and logical operators. Perform a few
+             operations and print the results.
+'''
+a=10
+b=5
+addition=a+b
+division=a/b
+print("Sum",addition,"Division",division)
+comparison=a>b
+print("Is a greater than b?:",comparison)
+equality=a==b
+print("Are a and b equal?",equality)
+logical_and=a and b
+logical_or=a or b
+print("Logical AND:",logical_and)
+print("Logical OR:", logical_or)
