351726106a923d849b1e3eb13d13f2b3d79970f7
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2024-10-25 14:13:04 +0530

    Add files via upload

---
+++ Program 9.py
@@ -0,0 +1,20 @@
+'''
+Author: Joshil S Abraham
+Date: 25 / 10 / 2024
+Description: Program to find the largest of three numbers.
+             The program should take three numbers as input from the user
+             and determine which one is the largest.
+             Use conditional statements to compare the numbers.
+'''
+first_number=int(input("Enter first number:"))
+second_number=int(input("Enter second number:"))
+third_number=int(input("Enter third number:"))
+if first_number>second_number and first_number>third_number:
+    print("The largest number is:",first_number)
+elif second_number>first_number and second_number>third_number:
+    print("The largest number is:",second_number)
+elif third_number>first_number and third_number>second_number:
+    print("The largest number is:",third_number)
+else:
+    print("Invalid")
+
