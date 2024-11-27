da561dc270f2f168b5b35cfbc38ce9aab93abe77
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2024-11-27 21:56:52 +0530

    Update largest and smallest.py

--- largest and smallest.py
+++ largest and smallest.py
@@ -12,3 +12,20 @@ while n<total:
         large=number
 print("Largest Number:",large)
 print("Smallest Number:",small)
+
+# To find the largest and smallest numbers from a given list of numbers
+total=int(input("Total number of numbers="))
+num1= int(input("Enter number:"))
+small=num1
+large=num1
+while total>1:
+    total=total-1
+    number=int(input("Enter number:"))
+    if number<small:
+        small=number
+    if number>large:
+        second_large=large
+        large=num1
+print("Largest Number:",large)
+print(second_large)
+print("Smallest Number:",small)
