bf24a6b4b2eae6e33e3caacdad4a6bbff89bf296
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2024-11-28 04:57:13 +0530

    Update Formatting.py

--- Formatting.py
+++ Formatting.py
@@ -1,6 +1,6 @@
 '''
 Author: Joshil S Abraham
-Date: 18 / 10 / 2024
+Date: 18/10/2024
 Description: Write a Python program to print the following string in a specific format:
 	             Sample String : "Twinkle, twinkle, little star, How I wonder what you are!
 	             		      Up above the world so high, Like a diamond in the sky.
