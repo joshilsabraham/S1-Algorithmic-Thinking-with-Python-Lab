dcb8e7685ec7326a991637b883118e03fe473f36
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2024-11-28 04:33:44 +0530

    Update Program 4.py

--- Program 4.py
+++ Program 4.py
@@ -1,18 +1,14 @@
 '''
 Author: Joshil S Abraham
 Date: 18 / 10 / 2024
-Description: Program that uses functions from the math module to
-             perform the following operations on a number provided
-             by the user:
-             Find the square root.
-             Calculate the factorial.
-             Raise the number to the power of 2.
+Description: Program to find the square root, calculate the factorial and raise the number 
+             to the power of 2 using functions from the math module
 '''
-import math
-number=int(input("Enter a number:"))
-square_root=math.sqrt(number)
-print("Square root of",number,':',square_root)
-factorial=math.factorial(number)
-print("Factorial of",number,':',factorial)
-exponentation=math.pow(number,2)
-print(number,"raised to power 2:",exponentation)
+import math    # Importing math module
+number = int(input("Enter a number:"))
+square_root = math.sqrt(number)    # Calculating the square root of the number
+print(f"Square root of {number}: {square_root}")
+factorial = math.factorial(number)    # Calculating the factorial of the number
+print(f"Factorial of {number}: {factorial}")
+exponentiation = math.pow(number,2)    # Raising the number to the power of 2
+print(number,"raised to power 2: ",exponentiation)
