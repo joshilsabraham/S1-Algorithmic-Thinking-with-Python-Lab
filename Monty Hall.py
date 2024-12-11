6ad4759be43941ace3d45d9a446f95ee2f31c6bb
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2024-12-11 23:17:20 +0530

    Update Monty Hall.py

--- Monty Hall.py
+++ Monty Hall.py
@@ -1,11 +1,11 @@
 '''
-Description: Suppose you're on a game show, and you are given the choice of three doors: Behind one door is a
-car; behind the others, goats. You pick a door, say No. 1, and the host, who knows what is behind the
-doors, opens another door, say No. 3, which has a goat. He then asks, "Do you want to pick door No.
-2?" Is it to your advantage to switch your choice?
-Author: Sarju S
-Date: November  13  2024
-Version: 1.0
+Author: Joshil S Abraham
+Date: 06/12/2024
+Description: Suppose you're on a game show, and you are given the choice of three doors: 
+             Behind one door is a car; behind the others, goats. You pick a door, say No. 1,
+             and the host, who knows what is behind the doors, opens another door, say No. 3, 
+             which has a goat. He then asks, "Do you want to pick door No. 2?" 
+             Is it to your advantage to switch your choice?
 '''
 import random
 
